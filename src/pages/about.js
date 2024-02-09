14ce4f6d3d13edcb287c655344ed90bd311cf273
林牧賢 <michael.lin@welly.tw>
2024-02-09 19:17:01 +0800

    新增檔案

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const AboutPage =()=>{
+    return (
+        <Layout pageTitle="About Me">
+            <p>This is my introduce!</p>
+        </Layout>
+    )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
