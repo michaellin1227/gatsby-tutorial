69caacca1396807ab4a691f8da44c5c4363eac9a
林牧賢 <michael.lin@welly.tw>
2024-02-13 04:52:15 +0800

    練習使用mdx

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -7,11 +7,15 @@ const Blog = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       <p>我的部落格頁面</p>
-      <ul>
-        {data.allFile.nodes.map((d) => {
-          return <li key={d.name}>{d.name}</li>
-        })}
-      </ul>
+      {data.allMdx.nodes.map((d) => {
+        return (
+          <article key={d.id}>
+            <h2>{d.frontmatter.title}</h2>
+            <p>Posted: {d.frontmatter.date}</p>
+            <p>{d.excerpt}</p>
+          </article>
+        )
+      })}
     </Layout>
   )
 }
@@ -20,9 +24,20 @@ export const Head = () => <Seo />
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM D, YYYY")
+          slug
+        }
+        id
+        excerpt
+        parent {
+          ... on File {
+            modifiedTime
+          }
+        }
       }
     }
   }
