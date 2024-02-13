e299198bdba700635a4fdbb05c1b106c46d7413c
林牧賢 <michael.lin@welly.tw>
2024-02-14 01:33:03 +0800

    處理dynamic-image

--- src/pages/blog/{mdx.frontmatter__slug}.jsx
+++ src/pages/blog/{mdx.frontmatter__slug}.jsx
@@ -2,10 +2,14 @@ import * as React from "react"
 import Layout from "../../components/Layout"
 import Seo from "../../components/Seo"
 import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const BlogPost = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
+  console.log(data.mdx.frontmatter.title)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
       <p>{data.mdx.frontmatter.date}</p>
       {children}
     </Layout>
@@ -17,11 +21,19 @@ export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
 export default BlogPost
 
 export const query = graphql`
-  query MyQuery($slug: String) {
-    mdx(frontmatter: { slug: { eq: $slug } }) {
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
